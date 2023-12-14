e7ca534188f1bbe5ef778e9c4b64a0e7d72d22dd
SamueBio <samuele.biondo.lavoro@gmail.com>
2023-12-14 23:01:24 +0100

    commit

--- app/src/main/java/com/example/myapplication/retrofit/RetrofitService.java
+++ app/src/main/java/com/example/myapplication/retrofit/RetrofitService.java
@@ -19,7 +19,7 @@ public class RetrofitService {
 
     private void initializeRetrofit() {
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://ec2-51-20-87-215.eu-north-1.compute.amazonaws.com:9000/")
+                .baseUrl("http://ec2-13-51-165-225.eu-north-1.compute.amazonaws.com:9000/")
                 .addConverterFactory(new NullOnEmptyConverterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
