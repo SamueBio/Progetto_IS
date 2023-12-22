0b7bd519590e3769853e344550441950c3fea1ae
ferrodavide <890710@stud.unive.it>
2023-12-22 17:20:24 +0100

    FavouritesApi

---
+++ app/src/main/java/com/example/myapplication/retrofit/FavouritesApi.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.retrofit;
+
+import com.example.myapplication.model.Favourite;
+import com.example.myapplication.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface FavouritesApi {
+    @GET("/Favourites/get-favouriteaccomodation-by-user")
+    Call<User> getFavouriteAccomodationByUser(@Body Favourite favourite);
+
+    @POST("/Favourites/save")
+    Call<User> save(@Body Favourite favourite);
+}
