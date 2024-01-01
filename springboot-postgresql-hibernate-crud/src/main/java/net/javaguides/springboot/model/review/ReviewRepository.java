80fd83e5010469a62ebdc61ae0d0485f5bab7850
Stefano <892316@stud.unive.it>
2024-01-01 13:35:15 +0100

    Happy New Year

--- springboot-postgresql-hibernate-crud/src/main/java/net/javaguides/springboot/model/review/ReviewRepository.java
+++ springboot-postgresql-hibernate-crud/src/main/java/net/javaguides/springboot/model/review/ReviewRepository.java
@@ -1,13 +1,17 @@
 package net.javaguides.springboot.model.review;
 
 import net.javaguides.springboot.model.accomodation.Accommodation;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ReviewRepository extends CrudRepository<Review,String> {
+    @Modifying
+    @Transactional
     @Query(value = "INSERT INTO reviews (username,accommodation,text,stars,date) VALUES (:#{#review.username},:#{#review.accommodation},:#{#review.text},:#{#review.stars},:#{#review.date})", nativeQuery = true )
     void saveReview(@Param("review") Review review);
 
